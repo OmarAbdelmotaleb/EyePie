40ea06a3571a34fe3416ee9f97e8b801282b5455
RolandTumbokon <55549129+RolandTumbokon@users.noreply.github.com>
2019-12-02 00:22:09 -0500

    Javascript stuff

---
+++ script.js
@@ -0,0 +1,23 @@
+//Javascript by Roland Tumbokon
+
+
+orders = document.getElementsByClassName("OrderItem")
+
+function opacity0() {
+  console.log(orders[i])
+  orders[i].style.color = "green"
+}
+
+var big = function() {
+  this.style.transform = "scale(1.0)"
+}
+
+var small = function() {
+  this.style.transform = "scale(1.1)"
+}
+
+for (var i = 0; i < orders.length; i++) {
+	orders[i].addEventListener("mouseover", small)
+	orders[i].addEventListener("mouseleave", big)
+}
+
